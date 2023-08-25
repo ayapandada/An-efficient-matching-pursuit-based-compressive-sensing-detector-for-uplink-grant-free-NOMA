a88c001c65b0a6a4096ff8f480e0d635ce2d2375
ayapandada <lishuobecky@gmail.com>
2023-08-25 17:14:51 +0800

    Update BCoSaMP_common.m

--- BCoSaMP_common.m
+++ BCoSaMP_common.m
@@ -1,13 +1,11 @@
 function [TAC_i,S_i]= BCoSaMP_common(H,M,Y,nTx_AS,nTx,nRx,N0,timeslot) 
-%M 调制阶数
-%H矩阵的维度为：nRx*nTx
 
 TAC_i=[];
 V_s=[];
 R_i=Y;
 K=nTx_AS;
 
-D=kron(H,eye(timeslot));   %向量化
+D=kron(H,eye(timeslot));  
 p=reshape(Y.',[],1);
 
 
@@ -29,7 +27,7 @@ for i=1:1:nTx_AS
     TAC_i = union(TAC_i,TAC_Can);   
     
     V_s=[V_s,V(1)];
-    [TAC_i,v]=sort(TAC_i,'ascend');    %对索引进行排序
+    [TAC_i,v]=sort(TAC_i,'ascend');  
     
     Dist_S=[];
     if length(TAC_i)<=nTx
@@ -37,19 +35,18 @@ for i=1:1:nTx_AS
     end
         W=pinv(H(:,TAC_i));
         H_i=H(:,TAC_i);
-        S_i=W*Y;                            %对应用户索引下的用户符号
+        S_i=W*Y;                         
         S_re=S_i.';
         S_all=zeros(timeslot,nTx);
         S_all(:,TAC_i)=S_re;
-        
-        %修剪集合
+
         for  j=1:1:2*nTx_AS
             B_S=norm(S_re(:,j),'fro')^2;
             Dist_S=[Dist_S,B_S];  
         end
            [V_Si,l_Si]=sort(Dist_S,'descend');
            TAC_i=TAC_i(l_Si(1:K));
-           [TAC_i,v]=sort(TAC_i,'ascend');    %对索引进行排序
+           [TAC_i,v]=sort(TAC_i,'ascend');   
            
            H_i=H(:,TAC_i);
            S_i_f=S_all(:,TAC_i);
